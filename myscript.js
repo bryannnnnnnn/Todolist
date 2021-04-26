11eaec697e6c470416e18c291ce8a14ffbe2b137
bryannnnnnnn <81378632+bryannnnnnnn@users.noreply.github.com>
2021-04-26 11:36:00 -0500

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -1,1 +1,11 @@
+document.getElementById("addBtn").addEventListener("click",addText);
 
+function addText(){
+  var listItem = document.createElement("li");
+  listItem.innerText = document.getElementById("myInput").value;
+  document.getElementById("myUl").appendChild(listItem);
+  var button = document.createElement("button");
+  button.innerText = "x";
+  listItem.appendChild(button);
+  
+  }
